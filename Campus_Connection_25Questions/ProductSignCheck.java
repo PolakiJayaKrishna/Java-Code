efbe0dddaa4557fcfacf990643eac3d56ac1b3d3
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-05-31 23:02:14 +0530

    8th Question

--- Campus_Connection_25Questions/ProductSignCheck.java
+++ Campus_Connection_25Questions/ProductSignCheck.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class ProductSignCheck{
+class ProductSignCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Three Numbers: ");
@@ -8,13 +8,15 @@ class ProductSignCheck{
         int Number2 = sc.nextInt();
         int Number3 = sc.nextInt();
         sc.close();
-        int Product = Number1*Number3*Number2;
-        if(Product>0){
-            System.out.print("Product of " +Number1 +","+Number2 +","+Number3 +" = "+Product + " is Positive...");
-        }else if(Product<0){
-            System.out.print("Product of " +Number1 +","+Number2 +","+Number3 +" = "+Product + " is Negative..");
-        }else{
-            System.out.print("Product of " +Number1 +","+Number2 +","+Number3 +" = "+Product +" is Zero...");
+        int Product = Number1 * Number3 * Number2;
+        if (Product > 0) {
+            System.out.print(
+                    "Product of " + Number1 + "," + Number2 + "," + Number3 + " = " + Product + " is Positive...");
+        } else if (Product < 0) {
+            System.out.print(
+                    "Product of " + Number1 + "," + Number2 + "," + Number3 + " = " + Product + " is Negative..");
+        } else {
+            System.out.print("Product of " + Number1 + "," + Number2 + "," + Number3 + " = " + Product + " is Zero...");
         }
     }
 }
