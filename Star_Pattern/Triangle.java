015c2bafce2584f652ea7373b6fe922d458c4376
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-05-31 20:53:55 +0530

    Add Triangle pattern printing

---
+++ Star_Pattern/Triangle.java
@@ -0,0 +1,14 @@
+package Star_Pattern;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        int n = 5; // Size of the triangle
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
