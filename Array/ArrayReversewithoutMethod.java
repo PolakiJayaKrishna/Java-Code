a64d067955b7a260d09262ef4552122c2557d5b2
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-02 23:13:16 +0530

    Add in-place array reversal program without using extra method

---
+++ Array/ArrayReversewithoutMethod.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayReversewithoutMethod {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Size of an array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Before Reversing: " + Arrays.toString(arr));
+        int j = size - 1, first, end;
+        for (int i = 0; i < size / 2; i++) {
+            first = arr[i];
+            end = arr[j];
+            arr[i] = end;
+            arr[j] = first;
+            j--;
+        }
+        System.out.println("Array After Reversing: " + Arrays.toString(arr));
+        sc.close();
+    }
+}
