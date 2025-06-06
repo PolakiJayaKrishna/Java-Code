0dba36dbdb365d04e61b00d80d431cd52e756fc7
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-06 23:44:57 +0530

    11th Question

---
+++ Campus_Connection_25Questions/LargestNumberAmongThree.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestNumberAmongThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b && b == c) {
+            System.out.println("a,b,c Numbers are same...");
+        } else {
+            if (a > b && a > c) {
+                System.out.print(a + " Bigger...value !");
+            } else if (b > a && b > a) {
+
+                System.out.print(b + " Bigger...value !");
+
+            } else if (c > a && c > b) {
+                System.out.print(c + " Bigger...value !");
+
+            }
+        }
+        sc.close();
+    }
+}
