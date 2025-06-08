b60b746da68dad4790129021eb9d4dad53af80ba
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-08 21:28:57 +0530

    KPattern

---
+++ Star_Pattern/KPattern.java
@@ -0,0 +1,20 @@
+
+package Star_Pattern;
+import java.util.Scanner;
+public class KPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j==0 || j==(i<N/2 ? N-1-i : i))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
