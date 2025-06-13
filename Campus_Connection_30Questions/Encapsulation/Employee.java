1ada268569d3ae26d064dc864b9c64cded0200e2
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-13 12:05:54 +0530

    Employee Annual Salary

--- Campus_Connection_30Questions/Encapsulation/Employee.java
+++ Campus_Connection_30Questions/Encapsulation/Employee.java
@@ -1,26 +1,33 @@
 //7. Create an Employee class with private fields and methods to calculate annual salary. Use proper accessors.
 package Encapsulation;
+
 public class Employee {
     private String name;
     private long salary;
-    Employee(String name , long salary){
+
+    Employee(String name, long salary) {
         this.salary = salary;
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
-    public long getSalary(){
+
+    public long getSalary() {
         return this.salary;
     }
-    public long annualsalary(){
-        return salary*12;
+
+    public long getAnnualSalary() {
+        return salary * 12;
     }
-    public void display(){
+
+    public void display() {
         System.out.println("Employee Name: " + getName());
         System.out.println("Salary of the Employee is: " + salary);
-        System.out.println("Annual Salary: "  + annualsalary());
+        System.out.println("Annual Salary: " + getAnnualSalary());
     }
+
     public static void main(String[] args) {
         Employee employee = new Employee("JayaKrishna", 52000);
         employee.display();
