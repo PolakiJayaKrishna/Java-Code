ef8fccfec61399fc68bba8c0a40551fcc89f48b8
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-22 22:24:29 +0530

    Fix matrix values and verify total sum output

---
+++ Array/MatrixSum3x3.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class MatrixSum3x3 {
+    // Print the sum of all elements in a 3x3 matrix.
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 0, 2 }, { 2, 0, 10 }, { 3, 0, -10 } };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
