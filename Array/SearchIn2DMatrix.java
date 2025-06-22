2a7abfe64bee8bc5593e0ee886211947cd17eb86
P.JayaKrishna <polakijayakrishna@gmail.com>
2025-06-22 23:27:49 +0530

    Add program to search an element in 2D array and print its index

---
+++ Array/SearchIn2DMatrix.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class SearchIn2DMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{1,0,3},{2,0,3}};
+        int num = 2;
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j <arr[i].length ; j++) {
+                if(arr[i][j] == num)
+                    System.out.print("Row " + i + " & Column " + j);
+            }
+        }
+    }
+}
