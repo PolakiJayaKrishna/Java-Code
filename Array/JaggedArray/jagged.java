8634fb4068d889ad00f1d19850bca71849b7c25e
Polaki Jaya Krishna <polakijayakrishna@gmail.com>
2025-06-28 21:06:04 +0530

    The above example shows how to create a jagged array, it takes an array with 2 rows, the first row has three elements and the second row has two elements. It fills the array with numbers starting from 0 and then prints them. This is the easiest way to handle uneven data in 2D array.

---
+++ Array/JaggedArray/jagged.java
@@ -0,0 +1,23 @@
+package Array.JaggedArray;
+
+public class jagged {
+    public static void main(String[] args) {
+        int row = 5;
+        int[][] arr = new int[row][];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new int[i + 1];
+        }
+        int count =0;
+        for (int j = 0; j < arr.length; j++) {
+            for (int k = 0; k < arr[j].length; k++) {
+                arr[j][k] = count++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            for (int k = 0; k < arr[j].length; k++) {
+                System.out.print(arr[j][k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
