7eac41052d4fccddfc312d4d05faa34c37d3932e
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-21 13:54:17 +0530

    Find LargestNumber in an array

--- Array/LargestNumber.java
+++ Array/LargestNumber.java
@@ -1,5 +1,11 @@
 public class LargestNumber {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,10,5,6,7,8};
+        int largetNumber=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > largetNumber)
+                largetNumber = arr[i];
+        }
+        System.out.println(largetNumber);
     }
 }
