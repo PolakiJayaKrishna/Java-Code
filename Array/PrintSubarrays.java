1714e23d469704d4af32cf4c2dff4fb1c7542226
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-21 15:00:12 +0530

    Add program to print all continuous subarrays of integer array

---
+++ Array/PrintSubarrays.java
@@ -0,0 +1,20 @@
+//Write a Java program to print all subarrays of the array {2, 4, 6, 8, 10}.
+//Note: A subarray is a continuous part of the array, so print every possible sequence of contiguous elements.
+public class PrintSubarrays {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+        subArray(arr);
+    }
+    public static void subArray(int[] arr){
+        for(int i=0;i<arr.length;i++){              //i->Start
+            for(int j=i;j<arr.length;j++){          //j->End
+                for(int k=i;k<=j;k++){              //Prints SubArray
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+}
