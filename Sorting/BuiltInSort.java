d0eb1366744657e855ccb018ebdee5709111c6c2
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-22 21:25:48 +0530

    Add BuiltInSort class to demonstrate Java's built-in array sorting functionality

---
+++ Sorting/BuiltInSort.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BuiltInSort {
+    public static void main(String[] args) {
+        int[] arr = {1,0,10,50,2,3,4,5};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
