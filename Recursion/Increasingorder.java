f713aa575777c8b5eb8df53a5b9512dbb281788a
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-23 17:32:28 +0530

    Add Increasingorder class to implement a recursive method for printing numbers in increasing order

---
+++ Recursion/Increasingorder.java
@@ -0,0 +1,14 @@
+package Recurrsion;
+
+public class Increasingorder {
+    public static void main(String[] args) {
+        int upto = 15;
+        call(upto , 1);
+    }
+    public static void call(int upto , int num){
+        if(upto < num)  return;
+        System.out.print(num + " ");
+        num++;
+        call(upto , num);
+    }
+}
