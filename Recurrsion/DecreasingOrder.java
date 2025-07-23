aed84acfbc2c8fcc1019a8b524871dc1cb2ad8d0
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-23 14:19:43 +0530

    Add DecreasingOrder class to implement a recursive method for printing numbers in decreasing order

---
+++ Recurrsion/DecreasingOrder.java
@@ -0,0 +1,16 @@
+package Recurrsion;
+
+public class DecreasingOrder {
+    public static void main(String[] args) {
+        int number = 10;
+        call(number);
+    }
+    public static void  call(int n){
+        if(n == 1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        call(n-1);
+    }
+}
