494c19274a83f28662cb3adcd9a37a51e711ed19
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-29 19:02:46 +0530

    Add Task4 class to demonstrate age-based access control

---
+++ ExceptionHandling/Task4.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int age = 15;
+        if (age < 18) {
+            System.out.println("Noting");
+        }
+        System.out.println("Access granted.");
+    }
+}
+
